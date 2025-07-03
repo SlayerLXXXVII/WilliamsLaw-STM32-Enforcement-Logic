ae28e33b738dd844a7b83ab188fddbadcb06f496
SlayerLXXXVII <m.c.williams1987@gmail.com>
2025-07-03 17:01:31 -0400

    Create actuator_driver.c

---
+++ src/actuator_driver.c
@@ -0,0 +1,9 @@
+#include "actuator_driver.h"
+
+void actuator_init(void) {
+    // Configure GPIO/PWM for piezo actuators
+}
+
+void set_psi(float psi) {
+    // Convert psi to PWM duty cycle or voltage
+}
